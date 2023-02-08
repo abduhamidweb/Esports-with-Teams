c6ce78e724088b1ca2fb201edc4178f3875ad492
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2023-02-08 20:31:53 +0500

    finished

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -9,29 +9,37 @@ const Games = () => {
         <section>
           <div className="container">
             <div className="wrapperGames ">
-            <div className="row">
-              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
-                <img src={rasm2} alt="img" />
-                <h2>663</h2>
-                <p>Happy Player</p>
-              </div>
-              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
-                <img src={rasm1} alt="img" />
-                <h2>12+</h2>
-                <p>Esports</p>
-              </div>
-              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
-                <img src={rasm1} alt="img" />
-                <h2>10+</h2>
-                <p>Games</p>
-              </div>
-              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
-                <img src={rasm1} alt="img" />
-                <h2>6</h2>
-                <p>Famous Games</p>
+              <div className="row">
+                <div className="col-lg-3 col-md-6 col-sm-12 ">
+                  <div className="cardGames">
+                    <img src={rasm2} alt="img" />
+                    <h2>663</h2>
+                    <p>Happy Player</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-12 ">
+                  <div className="cardGames">
+                    <img src={rasm1} alt="img" />
+                    <h2>12+</h2>
+                    <p>Esports</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-12 ">
+                  <div className="cardGames">
+                    <img src={rasm1} alt="img" />
+                    <h2>10+</h2>
+                    <p>Games</p>
+                  </div>
+                </div>
+                <div className="col-lg-3 col-md-6 col-sm-12 ">
+                  <div className="cardGames">
+                    <img src={rasm1} alt="img" />
+                    <h2>6</h2>
+                    <p>Famous Games</p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </section>
       </>
