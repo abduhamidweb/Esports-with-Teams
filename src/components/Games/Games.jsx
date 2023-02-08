e250074b9abe11267670992a9d43f3a48ea6fffb
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2023-02-08 20:26:50 +0500

    all project finished

--- src/components/Games/Games.jsx
+++ src/components/Games/Games.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import "./Games.css"
+import rasm1 from "../../images/gusi.svg";
+import rasm2 from "../../images/iconlar.svg"; 
 
 const Games = () => {
     return (
-        <>
-         <h2>Games</h2>   
-        </>
+      <>
+        <section>
+          <div className="container">
+            <div className="wrapperGames ">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
+                <img src={rasm2} alt="img" />
+                <h2>663</h2>
+                <p>Happy Player</p>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
+                <img src={rasm1} alt="img" />
+                <h2>12+</h2>
+                <p>Esports</p>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
+                <img src={rasm1} alt="img" />
+                <h2>10+</h2>
+                <p>Games</p>
+              </div>
+              <div className="col-lg-3 col-md-6 col-sm-12 cardGames">
+                <img src={rasm1} alt="img" />
+                <h2>6</h2>
+                <p>Famous Games</p>
+              </div>
+            </div>
+          </div>
+          </div>
+        </section>
+      </>
     );
 };
 
--- src/components/Subscribe/Subscibe.jsx
+++ src/components/Subscribe/Subscibe.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import "./Subsciribe.css"
 const Subscibe = () => {
     return (
-        <>
-         <h2>Subscript</h2>   
-        </>
+      <>
+        <section>
+          <div className="container">
+            <div className="subWrapper">
+              <h2 className='subTitle'>Subscribe And Membership our 4AG Esports</h2>
+              <form action="#" className='subForm'>
+                <input type="email" className='sunEmail' placeholder="Type your email here" />
+                <button className='subscribe'>SUBSCRIBE</button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </>
     );
 };
 
